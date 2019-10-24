843ace9386e70ebf09968374f7eb7961b4e35921
kavitanarwade <56721871+kavitanarwade@users.noreply.github.com>
2019-10-25 02:23:05 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,51 @@
-	const key = 'b6a2765a75f8c77f0b0da08c03ea337c';
+const key = 'b6a2765a75f8c77f0b0da08c03ea337c';
 
-	function weatherBallon( cityName ) {
-		fetch('https://api.openweathermap.org/data/2.5/weather?q='+ cityName +'&appid=' + key)  
-		.then(function(resp) { return resp.json() }) // Convert data to json
-		.then(function(data) {
-			drawWeather(data);
-		})
-		.catch(function() {
-			// catch any errors
-		});
+function weatherBallon( cityName ) {
+	fetch('https://api.openweathermap.org/data/2.5/weather?q='+ cityName +'&appid=' + key)  
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+		drawWeather(data);
+	})
+	.catch(function() {
+		// catch any errors
+	});
+}
+function drawWeather( d ) {
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	//var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+	var description = d.weather[0].description; 
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+	if( description.indexOf('rain') > 0 ) {
+		document.body.className = 'rainy';
+	} else if( description.indexOf('cloud') > 0 ) {
+		document.body.className = 'cloudy';
+	} else if( description.indexOf('sunny') > 0 ) {
+		document.body.className = 'sunny';
+	} else {
+		document.body.className = 'clear';
 	}
-	function drawWeather( d ) {
-	  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-		var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
-	  var description = d.weather[0].description; 
-		
-		document.getElementById('description').innerHTML = description;
-		document.getElementById('temp').innerHTML = celcius + '&deg;';
-		document.getElementById('location').innerHTML = d.name;
-	  
-	  if( description.indexOf('rain') > 0 ) {
-	  	document.body.className = 'rainy';
-	  } else if( description.indexOf('cloud') > 0 ) {
-	  	document.body.className = 'cloudy';
-	  } else if( description.indexOf('sunny') > 0 ) {
-	  	document.body.className = 'sunny';
-	  } else {
-	  	document.body.className = 'clear';
-	  }
-	}
+}
 
+function defaultDescription(){
+	document.getElementById('city_name').value = "";
+	document.body.className = "bg";
+	document.getElementById('description').innerHTML = "Welcome";
+	document.getElementById('temp').innerHTML = "TO";
+	document.getElementById('location').innerHTML = "weatherOO";
+}
+
+document.getElementById("city_name")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("show_weather").click();
+    }
+});
+
+
+window.onload = function() {
+	defaultDescription();
+}
